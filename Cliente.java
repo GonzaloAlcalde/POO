5d623fbac738d04e47396e3f66a58029d2efb23f
damianpavazza <damianpavazza@yahoo.com.ar>
2017-06-21 17:33:54 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -3,46 +3,6 @@ package sistemaPerezHnos;
 import javax.swing.table.DefaultTableModel;
 
 public class Cliente {
-	private String razonSocial;
-	private String CUIT;
-	private String direccion;
-	private String telefono;
-	private String email;
-	
-	public String getRazonSocial() {
-		return razonSocial;
-	}
-	public void setRazonSocial(String razonSocial) {
-		this.razonSocial = razonSocial;
-	}
-	
-	public String getCUIT() {
-		return CUIT;
-	}
-	public void setCUIT(String cUIT) {
-		CUIT = cUIT;
-	}
-	
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	public static DefaultTableModel buscar(String texto){
 		DefaultTableModel modelo;
 		
