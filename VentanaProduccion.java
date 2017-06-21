f2afa25987d5c1041791499326da2eb214085929
GTorrandella <torrandellag@gmail.com>
2017-06-21 16:03:28 -0300

    Update VentanaProduccion.java

--- VentanaProduccion.java
+++ VentanaProduccion.java
@@ -29,9 +29,10 @@ public class VentanaProduccion extends JFrame {
 		setTitle("P\u00E9rez Hnos. - Producci\u00F3n");
 		
 		home = inicio;
+		home.setVisible(false);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 408, 324);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -74,4 +75,3 @@ public class VentanaProduccion extends JFrame {
 	}
 
 }
-
