52c41c1814e4a2c6c92e5fb818e9f2bf909825b5
GTorrandella <torrandellag@gmail.com>
2017-06-21 11:09:29 -0300

    Update VentanaAdministracion.java

--- VentanaAdministracion.java
+++ VentanaAdministracion.java
@@ -28,7 +28,7 @@ public class VentanaAdministracion extends JFrame {
 		
 		home.setVisible(false);
 		
-		setTitle("Administraci\u00F3n");
+		setTitle("P\u00E9rez Hnos. - Administraci\u00F3n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
