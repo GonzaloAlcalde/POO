4deba0387f31d86c40e28b38ed4613b654543e33
Gonza <gonzaloalcalde96@gmail.com>
2017-06-26 15:11:20 -0300

    no message

--- sistemaPerezHnos/VentanaPedidoNuevo.java
+++ sistemaPerezHnos/VentanaPedidoNuevo.java
@@ -106,7 +106,7 @@ public class VentanaPedidoNuevo extends JDialog {
 				JButton okButton = new JButton("OK");
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						crearPedido();
+						Pedido.crearPedido(idCliente, textField_1);
 						setVisible(false);
 						ventanaAdministracion.vaciarTabla();
 						ventanaAdministracion.actualizarPedidos();
@@ -127,28 +127,6 @@ public class VentanaPedidoNuevo extends JDialog {
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
-		}
-	}
-
-	private void crearPedido(){
-		try {
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/TP_Objetos", "root", "1234");
-			
-			String detalle = textField_1.getText();
-			String fecha = "NOW()";
-			
-			//"insert into cliente (codigo, razon_social, CUIT, telefonos) VALUES (1, 'Perex S.A.', '2344455544', '0303456 (preguntar por pipo)');"
-		
-			String sql = "insert into pedidos (idCliente, detalles, fecha) VALUES ("+ idCliente +", '"+ detalle +"', "+ fecha +");";
-			
-			Statement st1 = conexion.createStatement();
-			st1.executeUpdate(sql);
-			
-			st1.close();
-			conexion.close();
-					
-		} catch (SQLException s){
-			s.printStackTrace();
 		}
 	}
 }
