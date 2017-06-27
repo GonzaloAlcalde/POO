93456319475bd40e94ac68bf1283a02878f0017e
GTorrandella <torrandellag@gmail.com>
2017-06-27 16:23:56 -0300

    Update VentanaAgregarComentario.java

--- sistemaPerezHnos/VentanaAgregarComentario.java
+++ sistemaPerezHnos/VentanaAgregarComentario.java
@@ -7,33 +7,28 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.DropMode;
+import java.awt.GridLayout;
 
 public class VentanaAgregarComentario extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			VentanaAgregarComentario dialog = new VentanaAgregarComentario();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Create the dialog.
-	 */
+	private JTextField comentario;
+	
 	public VentanaAgregarComentario() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		{
+			comentario = new JTextField();
+			comentario.setHorizontalAlignment(SwingConstants.CENTER);
+			contentPanel.add(comentario);
+			comentario.setColumns(50);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
